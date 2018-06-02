6ec3573b26ff4c215ee816f913f576c32056506e
codeChengeWord <38514444+codeChengeWord@users.noreply.github.com>
2018-06-02 18:55:29 +0800

    old

--- src/main/java/com/imooc/resository/GirlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imooc.resository;
-
-import com.imooc.domain.Girl;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface GirlRepository extends JpaRepository<Girl,Integer> {
-
-}
